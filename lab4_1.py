66b457a07648175e613646e3116519e8747a3e52
tasver <bohdannavrotskyi@gmail.com>
2018-09-10 12:47:30 +0300

    Create lab4_1.py

---
+++ lab4_1.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+import math
+print("Enter your numbers: ")
+a = float(input())
+b = float(input())
+x = (math.sqrt(a*b))/(math.e**a * b) + a * math.e**(2*a/b)
+
+print(f"x = {x}") 	
