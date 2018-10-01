8b620633826a5f62479ed1953302c396646b59b6
tasver <bohdannavrotskyi@gmail.com>
2018-10-01 15:01:20 +0300

    Update lab5_4.py

--- lab5_4.py
+++ lab5_4.py
@@ -1,25 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import math
-import cmath
+
 a, b, c = map(float, input('Enter 3 numbers: ').split())
-discr = b**2-4*a*c
-
-try:
-    discr_squere=math.sqrt(discr)
-except:
-    discr_squere=cmath.sqrt(discr)
-
-try:
-    if discr < 0:
-        x1=complex((-b+discr_squere)/(2*a))
-        x2=complex((-b-discr_squere)/(2*a))
-    elif discr == 0:
-        x1 = -b / (2*a)
-        x2 = -b / (2*a)
-    elif discr > 0:
-        x1 = (-b - discr_squere) / (2*a)
-        x2 = (-b + discr_squere) / (2*a)
-    print(f'X1={x1}\n X2={x2}')
-except ZeroDivisionError:
-    print('Error you can`t division on 0')
+if a == 0 and b != 0:
+    x1 = x2 = -(c / b)
+elif a == 0 and b == 0:
+    x1 = x2 = Any
+else:
+    discriminant = b ** 2 - 4 * a * c
+    x1 = (-b + discriminant ** 0.5)/2 * a
+    x2 = (-b - discriminant ** 0.5)/2 * a
+print(f'X1={x1}\n X2={x2}')
