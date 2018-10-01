ae8d2e7c8210ea8f2a59f5ac98920de6864a9f3c
tasver <bohdannavrotskyi@gmail.com>
2018-10-01 13:16:12 +0300

    Update lab5_4.py

--- lab5_4.py
+++ lab5_4.py
@@ -1,19 +1,25 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import sys
 import math
-print("Enter input data: ")
-a, b, c = map(float, input().split())
+import cmath
+a, b, c = map(float, input('Enter 3 numbers: ').split())
+discr = b**2-4*a*c
 
-discr = b**2 - 4*a*c;
-#print(f 'Discriminant = {discr}')
-if discr > 0:
-	x1 = (-b + math.sqrt(discr)) / (2*a)
-	x2 = (-b - math.sqrt(discr)) / (2*a)
-#	print(f ' x1 = {x1} x2 = {x2}')
-elif discr == 0:
-	x = -b/(2*a)
-	print (f 'x = {x}')
+try:
+    discr_squere=math.sqrt(discr)
+except:
+    discr_squere=cmath.sqrt(discr)
 
-#formula = a*x**2+b*x+c=0
-#print (a,b,c)
+try:
+    if discr < 0:
+        x1=complex((-b+discr_squere)/(2*a))
+        x2=complex((-b-discr_squere)/(2*a))
+    elif discr == 0:
+        x1 = -b / (2*a)
+        x2 = -b / (2*a)
+    elif discr > 0:
+        x1 = (-b - discr_squere) / (2*a)
+        x2 = (-b + discr_squere) / (2*a)
+    print(f'X1={x1}\n X2={x2}')
+except ZeroDivisionError:
+    print('Error you can`t division on 0')
