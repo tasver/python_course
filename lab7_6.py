d05aedb1c429355a04c3339ce222d7c5bce76058
tasver <bohdannavrotskyi@gmail.com>
2018-10-21 16:13:44 +0300

    Create lab7_6.py

---
+++ lab7_6.py
@@ -0,0 +1,20 @@
+
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+def input_str() -> str:
+	""" This function make input of string data"""
+	input_string = str(input('Enter your string: '))
+	return input_string
+
+def string_make_frame(string: str) -> str:
+	""" This function make  string"""
+	count_string = len(string)+4
+	
+	return f'\n{"*"*count_string} \n* {string} * \n{"*"*count_string}'
+
+	
+def output_str(string:str) -> str:
+    """ This function print result"""
+    print(string)
+
+output_str(string_make_frame(input_str()))
