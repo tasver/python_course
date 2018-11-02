e671207bf2cc9036dc5892d6b5b1587cd9101bef
tasver <bohdannavrotskyi@gmail.com>
2018-11-02 19:41:40 +0200

    Update lab9_2.py

--- lab9_2.py
+++ lab9_2.py
@@ -9,12 +9,12 @@ def input_data() -> float:
 def numbers_to_letters(number: float) -> str:
     if number >=1000000: 
         raise ValueError('Not supporter equal or over million')
-    numbers = {1: 'один', 2: 'два', 3: 'три', 4: 'чотири', 5: 'п\'ять', 
-    6: 'шість', 7: 'сім', 8: 'вісім', 9: 'дев\'ять', 10: 'десять', 
+    numbers = {1: "один", 2: "два", 3: "три", 4: "чотири", 5: "п\'ять", 
+    6: "шість", 7: "сім", 8: 'вісім', 9: 'дев\'ять', 10: 'десять', 
     11: 'одинадцять', 12: 'дванадцять', 13: 'тринадцять', 14: 'чотирнадцять', 
     15: 'п\'ятнадцять', 16: 'шістнадцять', 17: 'сімнадцять', 18: 'вісімнадцять',
     19: 'дев\'ятнадцять',20: 'двадцять', 30: 'тридцять', 40: 'сорок', 
-    50: 'п\'ятдесят', 60: 'шістдесят', 70: 'сімдесять', 80: 'вісімдесять', 
+    50: "п\'ятдесят", 60: 'шістдесят', 70: 'сімдесять', 80: 'вісімдесять', 
     90: 'дев\'яносто', 100: 'сто',200: 'двісті', 300: 'триста', 
     400: 'чотириста', 500: 'п\'ятсот', 600: 'шістсот',700: 'сімсот',
     800: 'вісімсот', 900: 'дев\'ятсот', 0: ''}
@@ -40,12 +40,13 @@ def numbers_to_letters(number: float) -> str:
         len_int_number=len(str(number_without_coins))
     if len_int_number==4:
         units_thousend = number_without_coins//1000
+        number_without_coins=number_without_coins-units_thousend*1000
         if dozens_thousend==1:
             letters_numbers.append(numbers[int(dozens_thousend)*10+int(units_thousend)])
             units_thousend=10000+units_thousend
         else:
             letters_numbers.append(numbers[int(units_thousend)])
-        number_without_coins=number_without_coins-units_thousend*1000
+        
         len_int_number=len(str(number_without_coins))
     
         if int(units_thousend)==1:
@@ -59,6 +60,7 @@ def numbers_to_letters(number: float) -> str:
         elif int(units_thousend)>5:
             letters_numbers.append("тисяч")
         else: letters_numbers.append("тисяч")
+    else: letters_numbers.append("тисяч")
 
     """units+hryvna"""  
     if len_int_number==3:
@@ -73,13 +75,14 @@ def numbers_to_letters(number: float) -> str:
         number_without_coins=number_without_coins-dozens*10
         len_int_number=len(str(number_without_coins))
     if len_int_number==1:
-        units= number_without_coins//1
+        units=number_without_coins//1
+        number_without_coins=number_without_coins-units
         if dozens==1:
             letters_numbers.append(numbers[int(dozens)*10+int(units)])
             units=10+units
         else:
             letters_numbers.append(numbers[int(units)])
-        number_without_coins=number_without_coins-units
+        
         len_int_number=len(str(number_without_coins))
     
     if int(units)==1:
@@ -90,8 +93,10 @@ def numbers_to_letters(number: float) -> str:
         if "два" in letters_numbers:
             letters_numbers[letters_numbers.index("два")] = "дві"
         letters_numbers.append("гривні")
+    elif int(units)>5:
+            letters_numbers.append("гривень")
     else: 
-        letters_numbers.append("гривень") 
+        letters_numbers.append("гривень")
         
     """coins"""
     coins_dozens = coins_list.pop(0)
