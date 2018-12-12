b8e96c2d509e0f8c3954c735bc1e0c1f12150013
tasver <bohdannavrotskyi@gmail.com>
2018-12-12 18:38:17 +0200

    Rename lab15_1_server.py to lab15_1_client.py

---
+++ lab15_1_client.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = '127.0.0.1'
+port = 9999
+
+s.connect((host, port))
+
+while True:
+    server_responce = s.recv(1024)
+    print(server_responce.decode('utf-8'))
+    a = input()
+    s.send(a.encode('utf-8'))
+s.close()
